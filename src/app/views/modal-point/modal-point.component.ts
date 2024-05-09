9c255a317c278cfe27f36a407391fe2c08eb2b06
lucas russin <lucas.russin@atos.net>
2024-05-09 16:11:03 -0300

    fix: att tela modal

--- src/app/views/modal-point/modal-point.component.ts
+++ src/app/views/modal-point/modal-point.component.ts
@@ -1,14 +1,17 @@
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-modal-point',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgStyle, IconDirective],
   templateUrl: './modal-point.component.html',
   styleUrl: './modal-point.component.scss'
 })
 export class ModalPointComponent {
+
+  balanceValue: string = '20.245,00'; // Defina o valor manualmente aqui
 
   @Input() isModalOpen: boolean = false;
   @Output() closeModal = new EventEmitter<void>();
@@ -16,4 +19,30 @@ export class ModalPointComponent {
   onCloseModal() {
     this.closeModal.emit();
   }
+
+  get circleColor(): string {
+    if (this.balanceValue <= '20.244,99') {
+      return 'var(--danger)';
+    } else if (this.balanceValue >= '20.245,00' && this.balanceValue <= '27.477,50') {
+      return 'var(--warning-alert-1)';
+    } else if (this.balanceValue >= '27.477,50') {
+      return 'var(--success)';
+    }
+    else {
+      return 'transparent'
+    }
+  }
+
+  get circleTooltip(): string {
+    if (this.balanceValue <= '20.244,99') {
+      return 'Limite Inferior';
+    } else if (this.balanceValue >= '20.245,00' && this.balanceValue <= '27.477,50') {
+      return 'Mediano';
+    } else if (this.balanceValue >= '27.477,50') {
+      return 'Limite Superior';
+    } else {
+      return 'Tooltip padrão';
+    }
+  }
+
 }
