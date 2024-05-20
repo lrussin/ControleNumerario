f7f012b9eb7b8a13e573870f2b07788ddf931e5f
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2024-05-20 16:01:07 -0300

    ajustando home.com,ponent.ts

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './service/home.service';
 import { IconDirective } from '@coreui/icons-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [IconDirective],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   data: any;
@@ -15,14 +14,16 @@ export class HomeComponent implements OnInit {
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
-    this.homeService.GetAllPA().subscribe(
-      response => {
-        this.data = response;
-        console.log(this.data);
+    console.log('Iniciando conponenmt')
+    this.homeService.GetAllPA().subscribe({
+      next: (response) =>{
+          this.data = response;
+          console.log(this.data);
+        
       },
-      error => {
+      error: (error) =>{
         console.error('Erro ao buscar dados', error);
       }
-    );
+    });
   }
 }
