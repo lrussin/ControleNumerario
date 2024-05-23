f6a382da3e4f0a9eafec77daa65504e8a3fb9aa1
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2024-05-23 19:38:52 -0300

    testando busca de usuarios

--- src/app/views/users-list/users-list.component.ts
+++ src/app/views/users-list/users-list.component.ts
@@ -1,59 +1,83 @@
 import { ModalRegisterComponent } from './../modal-register/modal-register.component';
-import { NgIf } from '@angular/common';
-import { Component, OnInit,EventEmitter, Input, Output } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { IconDirective } from '@coreui/icons-angular';
-import { RegisterComponent } from '../pages/register/register.component';
-import {UsersListService} from './Service/users-list.service';
+import { UsersListService } from './Service/users-list.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [IconDirective, MatButtonModule, MatIconModule, NgIf, ModalRegisterComponent],
+  imports: [IconDirective, MatButtonModule, MatIconModule, NgIf, ModalRegisterComponent,NgFor,FormsModule],
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+  styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
-users:any;
+  users: any;
   pageNumber = 1;
-  pageSize = 100;
+  pageSize = 10;
   isModalOpen: boolean = false;
+  isLoading: boolean = false;
+  error: string | null = null;
+  filteredUsers:string = '';
+  searchTerm:string = '';
 
-  constructor(private userslistService:UsersListService){}
+  constructor(private userslistService: UsersListService) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.loadData();
   }
-  loadData():void{
-    console.log("Ta chegando SAporra?")
-    this.userslistService.GetAllUsers(this.pageNumber,this.pageSize).subscribe({
 
-      next: (response) =>{
+  loadData(): void {
+    this.userslistService.GetAllUsers(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
         this.users = response;
-        console.log(this.users);
+        this.filteredUsers = response; // Initialize filteredUsers
       },
-      error: (error) =>{
+      error: (error) => {
         console.error('Erro ao buscar dados', error);
       }
     });
   }
+  searchUsers(): void {
+    if (this.searchTerm) {
+      this.filteredUsers = this.users.filter((user: any) =>
+        user.descNomeUsuario.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredUsers = this.users;
+    }
+  }
 
-  nextPage():void{
-    this.pageNumber ++;
+  nextPage(): void {
+    this.pageNumber++;
     this.loadData();
   }
-  prevPage():void{
-    if (this.pageNumber > 1){
-      this.pageNumber --;
+
+  prevPage(): void {
+    if (this.pageNumber > 1) {
+      this.pageNumber--;
     }
     this.loadData();
   }
-  openModal() {
+
+  setPage(page: number): void {
+    this.pageNumber = page;
+    this.loadData();
+  }
+
+  openModal(): void {
     this.isModalOpen = true;
   }
 
-  closeModal() {
+  closeModal(): void {
     this.isModalOpen = false;
+  }
+
+  get pagesArray(): number[] {
+    return Array(Math.ceil(this.users.length / this.pageSize)).fill(0).map((x, i) => i + 1);
   }
 }
