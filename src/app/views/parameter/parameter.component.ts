af920132af4b25e022978e920d954670eae8f51f
lucas russin <lucas.russin@atos.net>
2024-05-27 17:43:27 -0300

    Fix: atualização do parameter somente o css html

--- src/app/views/parameter/parameter.component.ts
+++ src/app/views/parameter/parameter.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { ModalParameterComponent } from './../modal-parameter/modal-parameter.component';
 import { Component } from '@angular/core';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-parameter',
   standalone: true,
-  imports: [],
+  imports: [IconDirective, ModalParameterComponent, NgFor, CommonModule],
   templateUrl: './parameter.component.html',
   styleUrl: './parameter.component.scss'
 })
 export class ParameterComponent {
 
+  isModalOpen: boolean = false;
+  isEditMode: boolean = false;
+  currentUser: any | null = null;
+
+  openModal() {
+    this.isEditMode = false;
+    this.currentUser = null;
+    this.isModalOpen = true;
+  }
+
+  openEditModal(user: any) {
+    this.isEditMode = true;
+    this.currentUser = user;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
