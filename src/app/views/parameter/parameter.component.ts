729910f5a7cfe367119ab091b9ee18ff2cb96587
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2024-05-27 19:20:34 -0300

    resolvendo conflitos

--- src/app/views/parameter/parameter.component.ts
+++ src/app/views/parameter/parameter.component.ts
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import { IconDirective } from '@coreui/icons-angular';
 import { ModalRegisterComponent } from './../modal-register/modal-register.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { NgFor, NgIf } from '@angular/common';
@@ -9,69 +8,55 @@ import { FormsModule } from '@angular/forms';
 import { ParameterService } from './Service/parameter.service';
 import { ParameterList } from 'src/app/util/interfaces/ParameterList';
 
-=======
-import { CommonModule, NgFor } from '@angular/common';
-import { ModalParameterComponent } from './../modal-parameter/modal-parameter.component';
-import { Component } from '@angular/core';
-import { IconDirective } from '@coreui/icons-angular';
->>>>>>> af920132af4b25e022978e920d954670eae8f51f
 
 @Component({
   selector: 'app-parameter',
   standalone: true,
-<<<<<<< HEAD
-  imports: [IconDirective, MatButtonModule, MatIconModule, NgIf, ModalRegisterComponent,NgFor,FormsModule],
-=======
-  imports: [IconDirective, ModalParameterComponent, NgFor, CommonModule],
->>>>>>> af920132af4b25e022978e920d954670eae8f51f
+  imports: [IconDirective, MatButtonModule, MatIconModule, NgIf, ModalRegisterComponent, NgFor, FormsModule],
   templateUrl: './parameter.component.html',
   styleUrls: ['./parameter.component.scss']
 })
-export class ParameterComponent implements OnInit{
 
-<<<<<<< HEAD
-param:ParameterList[] = [];
-pageNumber = 1;
-pageSize = 10;
+export class ParameterComponent implements OnInit {
 
+  param: ParameterList[] = [];
+  pageNumber = 1;
+  pageSize = 10;
+  isModalOpen = false;
+  isEditMode = false;
+  currentUser: any;
 
-constructor(private parameterService:ParameterService){}
+  constructor(private parameterService: ParameterService) { }
 
-ngOnInit(): void {
-  this.loadData();
-}
+  ngOnInit(): void {
+    this.loadData();
+  }
 
-loadData():void{
-  this.parameterService.GetAllParams(this.pageNumber,this.pageSize).subscribe({
+  loadData(): void {
+    this.parameterService.GetAllParams(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        this.param = response;
+        console.log(this.param);
+      },
+      error: (error) => {
+        console.error('Erro ao buscar dados', error);
+      }
+    });
+  }
 
-    next: (response) =>{
-      this.param = response;
-      console.log(this.param);
-    },
-    error:  (error) => {
-      console.error('Erro ao buscar dados', error);
-    }
-  })
-}
-=======
-  isModalOpen: boolean = false;
-  isEditMode: boolean = false;
-  currentUser: any | null = null;
-
-  openModal() {
+  openModal(): void {
+    this.isModalOpen = true;
     this.isEditMode = false;
     this.currentUser = null;
-    this.isModalOpen = true;
   }
 
-  openEditModal(user: any) {
+  openEditModal(user: any): void {
+    this.isModalOpen = true;
     this.isEditMode = true;
     this.currentUser = user;
-    this.isModalOpen = true;
   }
 
-  closeModal() {
+  closeModal(): void {
     this.isModalOpen = false;
   }
->>>>>>> af920132af4b25e022978e920d954670eae8f51f
 }
